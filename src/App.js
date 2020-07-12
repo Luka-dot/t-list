446570b80f77294a89a9be7825862e85af26bc64
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-07-11 21:35:59 -0400

    slight adjustments for VS settings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-import Counter from './containers/Counter/Counter';
+import MainView from './containers/mainView/mainView';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Counter />
+       <MainView />
       </div>
     );
   }
